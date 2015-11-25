af41a902a9e381cbb2a5eaacb4c2e8ba90101e7b
paulopr4 <paulo.pr4@hotmail.com>
2015-11-25 08:50:09 -0200

    formatPaper
    calcular  melhor aproveitamento

---
+++ FormatPaper/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  FormatPaper
+//
+//  Created by FredKopeika on 24/11/15.
+//  Copyright © 2015 Paulopr4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var larguraPapel: UITextField!
+    @IBOutlet weak var AlturaPapel: UITextField!
+    @IBOutlet weak var larguraMaterial: UITextField!
+    @IBOutlet weak var alturaMaterial: UITextField!
+    @IBOutlet weak var Resposta: UILabel!
+    
+    
+    
+    
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        
+    }
+ 
+    
+    @IBAction func CalcularPapel(sender: AnyObject) {
+        
+        let calcular = Int(larguraPapel.text!
+        
+        
+        
+        }
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
